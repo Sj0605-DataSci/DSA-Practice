9f29cd642478a80cdf9ce757bb28da2580270001
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-04-04 01:00:51 +0530

    Problem 2 : Array and Hashing

---
+++ Day 1/is_anagram.py
@@ -0,0 +1,42 @@
+'''
+
+EASY
+ 
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging 
+the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+'''
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) = = Counter(t)
+        
+        
+        #return sorted(s) == sorted(t)   ( solution 2)
+
+
+        #( solution 1)
+
+        # if len(s) != len(t):
+        #     return False
+
+        # countS, countT = {}, {}
+
+        # for i in range(len(s)):
+        #     countS[s[i]] = 1 + countS.get(s[i], 0)
+        #     countT[t[i]] = 1 + countT.get(t[i], 0)
+        # return countS == countT
