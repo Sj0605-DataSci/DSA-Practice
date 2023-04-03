d38432fc2af802dc64189151218f26c255dd67a1
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-04-04 02:09:28 +0530

    commit

---
+++ Array_and_Hashing(9Q)/group_anagrams.py
@@ -0,0 +1,32 @@
+'''
+
+MEDIUM
+ 
+Given an array of strings strs, group the anagrams together. 
+You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+
+'''
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
