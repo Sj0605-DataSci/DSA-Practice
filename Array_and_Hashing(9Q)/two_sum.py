f9594331720f61323b586173dcf9ad202fa30369
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-04-04 01:32:06 +0530

    problem 3 : Array and Hashing

---
+++ Array_and_Hashing(9Q)/two_sum.py
@@ -0,0 +1,29 @@
+'''
+
+EASY
+ 
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
