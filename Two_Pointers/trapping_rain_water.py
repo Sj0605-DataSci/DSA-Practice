52514925bb4dab2881dfa4f71a9a1d5c2096d854
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-04-09 16:45:11 +0530

    commit

--- Two_Pointers/trapping_rain_water.py
+++ Two_Pointers/trapping_rain_water.py
@@ -15,6 +15,23 @@ Explanation: The above elevation map (black section)
 is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
 In this case, 6 units of rain water (blue section) are being trapped.
 
-
-
 '''
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
