523aedfa48056428fe58747da38da3380a48181d
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-04-14 00:26:03 +0530

    commit

--- Array_and_Hashing/longest_consecutive_sequence.py
+++ Array_and_Hashing/longest_consecutive_sequence.py
@@ -15,7 +15,7 @@ Input: nums = [100,4,200,1,3,2]
 
 Output: 4
 
-Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
 '''
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
